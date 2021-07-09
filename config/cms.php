fec885398f52167c20334b0ca5fb29cc83597b25
Irving Rodríguez Romero <irvingrodriguezromero@gmail.com>
2021-07-09 15:17:06 -0500

    CompraSublime implementacion del dashboard del administrador, creacion de la pagina y funcionalidad

---
+++ config/cms.php
@@ -0,0 +1,11 @@
+<?php
+return [
+'name' => 'CompraUTD' ,
+'currency' => '$' ,
+'company_phone' => '6181898699' ,
+'map' => 'Durango, Dgo' ,
+'maintenance_mode' => '0' ,
+'products_per_page' => '5' ,
+'products_per_page_random' => '10' ,
+]
+?>
